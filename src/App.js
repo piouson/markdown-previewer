2f3650eafcf5640b3c3c5572b4b3b5b9cf3ae496
piouson <piouson@gmail.com>
2020-06-01 23:08:51 +0100

    Add markdown parsing feature
    - Implement Marked library
      - Github Flavoured Markdown
      - Parse line breaks as `<br>`
      - Smart lists
      - Smart punctuations
      - No Ids for headers
    - Implement DOMPurify
      - Sanitize HTML from Marked
    - Implement localStorage
    - Add default text
    - Parse defaults on first load
    
    Resolve #3, resolve #5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import useMarked from './useMarked';
+import useLocalStorage from './useLocalStorage';
+
+const MD = `# Markdown Previewer Heading
+\`\`\`javascript
+const hello = 'Hello, World!';
+console.log(hello);
+\`\`\``;
 
 function App() {
+  const [text, setText] = useLocalStorage('marked', '');
+  const previewer = useMarked(text);
+
+  useEffect(() => {
+    setText(str => str ? str : MD);
+    console.log('App useEffect');
+  }, [setText]);
+
+  const handleChange = event =>
+    setText(event.target.value);
+
   return (
     <div className="App">
       <div id="nav">
@@ -9,9 +28,10 @@ function App() {
         <a href="#preview">Preview</a>
       </div>
       <div id="md">
-        <textarea id="editor" />
+        <textarea id="editor" value={text} onChange={handleChange} />
       </div>
       <div id="preview">
+        <div dangerouslySetInnerHTML={previewer} />
       </div>
     </div>
   );
