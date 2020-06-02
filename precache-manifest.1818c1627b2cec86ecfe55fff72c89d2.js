a715ca4c83a6fbdfd353cc012e970b7332f97446
piouson <piouson@gmail.com>
2020-06-02 01:19:59 +0100

    Updates

---
+++ precache-manifest.1818c1627b2cec86ecfe55fff72c89d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d289bc20350c91c0bef55bfed6939741",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "dc5e765c5163cb020d68",
+    "url": "/markdown-previewer/static/css/main.10490b3a.chunk.css"
+  },
+  {
+    "revision": "39a61ee6a2568cf9b724",
+    "url": "/markdown-previewer/static/js/2.5b9478fb.chunk.js"
+  },
+  {
+    "revision": "cbbbb0b9131a638ba1fa969274b2219c",
+    "url": "/markdown-previewer/static/js/2.5b9478fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5e765c5163cb020d68",
+    "url": "/markdown-previewer/static/js/main.a8a5dfb5.chunk.js"
+  },
+  {
+    "revision": "35deca1419128cdfce54",
+    "url": "/markdown-previewer/static/js/runtime-main.5fc24b94.js"
+  }
+]);
